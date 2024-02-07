24b0da415926171c6328b68f89db18bc15f0c214
JeHeeYu <sees1234558@gmail.com>
2024-02-07 10:50:33 +0900

    build: CMake Add OpenCV, CUDA, TensorRT build environments

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 
+#include "class_timer.hpp"
+#include "class_detector.h"
+
+#include "class_yolo_detector.hpp"
+
+#include "cuda_runtime_api.h"
+
+#include "NvInferRuntimeCommon.h".h"
+
+#define YOLO_CFG "D:\\Workstations\\TensorRTSP\\TensorRTSP\\yolo\\yolov4.cfg"
+#define YOLO_WEIGHTS "D:\\Workstations\\TensorRTSP\\TensorRTSP\\yolo\\yolov4.weights"
+
 int main(int argc, char *argv[])
 {
 #if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
@@ -20,6 +32,17 @@ int main(int argc, char *argv[])
         },
         Qt::QueuedConnection);
     engine.load(url);
+
+    Config config_v4;
+    std::unique_ptr<Detector> detector{ new Detector() };
+
+    config_v4.net_type = YOLOV4;
+    config_v4.file_model_cfg = YOLO_CFG;
+    config_v4.file_model_weights = YOLO_WEIGHTS;
+    config_v4.calibration_image_list_file_txt = "../configs/calibration_images.txt";
+    config_v4.inference_precison = FP32;
+    config_v4.detect_thresh = 0.5;
+    detector->init(config_v4);
 
     return app.exec();
 }
